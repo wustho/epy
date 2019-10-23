00cbd0d665444da636ca11df0b77c50e51e98f27
Benawi Adha <43810055+wustho@users.noreply.github.com>
2019-10-23 22:54:29 +0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from setuptools import setup
+from epr import __version__, __author__, __url__, __license__
+
+setup(
+    name = "epy",
+    version = __version__,
+    description = "Terminal/CLI Epub Reader (Fork of https://github.com/wustho/epr)",
+    url = __url__,
+    author = __author__,
+    license = __license__,
+    keywords = ["EPUB", "EPUB3", "CLI", "Terminal", "Reader"],
+    install_requires = ["windows-curses"] if sys.platform == "win32" else [],
+    python_requires = "~=3.0",
+    py_modules = ["epy"],
+    entry_points = { "console_scripts": ["epy = epy:main"] }
+)
