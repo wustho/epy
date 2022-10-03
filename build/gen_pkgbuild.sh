4ea3f6feea52eaee176e226b9fc1ee88e5a35f0f
Benawi Adha <benawiadha@gmail.com>
2022-10-03 07:08:08 +0700

    Initial fix, still needs testing

--- build/gen_pkgbuild.sh
+++ build/gen_pkgbuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -
+# https://wiki.archlinux.org/title/Python_package_guidelines
 
 (
 cat << 'EOF'
@@ -10,7 +11,7 @@ _name=epy
 provides=('epy')
 pkgver=EPY_PKGVER
 pkgrel=1
-pkgdesc="CLI Ebook Reader"
+pkgdesc="TUI Ebook Reader"
 arch=('any')
 url='https://github.com/wustho/epy'
 license=("GPL3")
@@ -20,25 +21,27 @@ depends=(
 )
 makedepends=(
   'git'
-  'python-setuptools'
+  'python-build'
+  'python-installer'
+  'python-wheel'
 )
 source=("git+https://github.com/wustho/$_name.git")
 sha256sums=('SKIP')
 
 pkgver() {
   cd "$_name"
-  printf "%s.r%s.%s" "$(grep -F '__version__ =' epy.py | awk -F\" '{print $2}')" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
+  printf "%s.r%s.%s" "$(grep -F '__version__ =' src/epy_reader/__init__.py | awk -F\" '{print $2}')" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
 }
 
 build() {
-    cd $_name
-    python setup.py build
+    cd "$_name"
+    python -m build --wheel --no-isolation
 }
 
 package() {
     install -D "$srcdir/$_name/LICENSE" "$pkgdir/usr/share/licenses/$pkgname"
-    cd $_name
-    python setup.py install --root="$pkgdir" --optimize=1 --skip-build
+    cd "$_name"
+    python -m installer --destdir="$pkgdir" dist/*.whl
 }
 EOF
-) | sed 's/pkgver=EPY_PKGVER/pkgver='`printf "%s.r%s.%s" "$(grep -F '__version__ =' epy.py | awk -F\" '{print $2}')" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"`'/'
+) | sed 's/pkgver=EPY_PKGVER/pkgver='`printf "%s.r%s.%s" "$(grep -F '__version__ =' src/epy_reader/__init__.py | awk -F\" '{print $2}')" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"`'/'
