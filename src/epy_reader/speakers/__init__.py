91b5fca3cc908096fca2d8eb27e91c42d2513b6b
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2023-03-27 21:44:41 +0200

    add speaker support for gtts-cli | mpv

--- src/epy_reader/speakers/__init__.py
+++ src/epy_reader/speakers/__init__.py
@@ -2,8 +2,10 @@ __all__ = [
     "SpeakerBaseModel",
     "SpeakerMimic",
     "SpeakerPico",
+    "SpeakerGttsMPV"
 ]
 
 from epy_reader.speakers.base import SpeakerBaseModel
 from epy_reader.speakers.mimic import SpeakerMimic
 from epy_reader.speakers.pico import SpeakerPico
+from epy_reader.speakers.gtts_mpv import SpeakerGttsMPV
